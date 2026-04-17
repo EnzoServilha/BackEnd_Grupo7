28af9a8a1313a1107cbf909751767e651767c09a
EnzoServilha <enzo.servilha@sptech.school>
2026-04-17 14:22:32 -0300

    adicionando endpoints,repository, service, exception e controller Permissão

---
+++ src/main/java/sptech/school/controller/PermissaoController.java
@@ -0,0 +1,53 @@
+package sptech.school.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sptech.school.dto.permissao.PermissaoRequestDto;
+import sptech.school.dto.permissao.PermissaoResponseDto;
+import sptech.school.service.PermissaoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/permissoes")
+public class PermissaoController {
+    @Autowired
+    private PermissaoService permissaoService;
+
+    @PostMapping
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<PermissaoResponseDto> criar(@RequestBody @Valid PermissaoRequestDto dto) {
+        return ResponseEntity.status(201).body(permissaoService.criar(dto));
+    }
+
+    @GetMapping
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<List<PermissaoResponseDto>> listarTodos() {
+        List<PermissaoResponseDto> permissoes = permissaoService.listarTodos();
+        if (permissoes.isEmpty()) return ResponseEntity.status(204).build();
+        return ResponseEntity.ok(permissoes);
+    }
+
+    @GetMapping("/{id}")
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<PermissaoResponseDto> buscarPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(permissaoService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<PermissaoResponseDto> atualizar(@PathVariable Integer id, @RequestBody @Valid PermissaoRequestDto dto) {
+        return ResponseEntity.ok(permissaoService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        permissaoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
