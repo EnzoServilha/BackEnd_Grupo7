4ac6656c4eb4a02e6bcdee5b88523e6e17fb4f2b
EnzoServilha <enzo.servilha@sptech.school>
2026-05-24 19:36:37 -0300

    feat: Testes unitarios de Fornecedor Service

---
+++ src/test/java/sptech/school/service/FornecedorServiceTest.java
@@ -0,0 +1,88 @@
+package sptech.school.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FornecedorServiceTest {
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void buscarPorId() {
+    }
+
+    @Test
+    void buscarPorNomeContato() {
+    }
+
+    @Test
+    void buscarPorNomeEmpresa() {
+    }
+
+    @Test
+    void listarPorCategoria() {
+    }
+
+    @Test
+    void listarPorMarca() {
+    }
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void deletar() {
+    }
+
+    @Test
+    void preencher() {
+    }
+
+    @Test
+    void testListar() {
+    }
+
+    @Test
+    void testBuscarPorId() {
+    }
+
+    @Test
+    void testBuscarPorNomeContato() {
+    }
+
+    @Test
+    void testBuscarPorNomeEmpresa() {
+    }
+
+    @Test
+    void testListarPorCategoria() {
+    }
+
+    @Test
+    void testListarPorMarca() {
+    }
+
+    @Test
+    void testCriar() {
+    }
+
+    @Test
+    void testAtualizar() {
+    }
+
+    @Test
+    void testDeletar() {
+    }
+
+    @Test
+    void testPreencher() {
+    }
+}
